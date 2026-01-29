7e2f66aa468c32ab0c95a68dbe47ba83613c8459
advait94 <adgautam94@gmail.com>
2026-01-30 00:38:29 +0530

    chore: Suppress TS error in next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /* config options here */
+  // @ts-expect-error - eslint config missing in type
   eslint: {
     ignoreDuringBuilds: true,
   },
